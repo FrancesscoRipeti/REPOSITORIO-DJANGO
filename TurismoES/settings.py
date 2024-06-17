68a10055ee96e4a83a1f89a1c6f44b0f0eb9ba7c
FrancesscoRipeti <fr.ripeti@duocuc.cl>
2024-06-17 00:24:55 -0400

    pc
    integracion de bootstrap

--- TurismoES/settings.py
+++ TurismoES/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'turismo',
     'calculadora',
+    'animales',
 
 ]
 
---
+++ animales/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index_ani', views.index_ani, name='index_ani'),
+    path('tienda_ani', views.tienda_ani, name='tienda_ani'),
+    path('adopcion_ani', views.adopcion_ani, name='adopcion_ani'),
+    path('nosotros_ani', views.nosotros_ani, name='nosotros_ani'),
+    path('contacto_ani', views.contacto_ani, name='contacto_ani'),
+    path('reg_ini_sesion_ani', views.reg_ini_sesion_ani, name='reg_ini_sesion_ani'),
+    path('pago_ani', views.pago_ani, name='pago_ani'),
+]
